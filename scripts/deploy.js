82d62180dcb21100bc005d8305d5241e5080a897
Gen Tamura <gen.tamura84@gmail.com>
2022-04-17 20:22:09 +0900

    Update deploy.js argments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,9 +4,9 @@ const main = async () => {
   const gameContract = await gameContractFactory.deploy(
     ['ZORO', 'NAMI', 'USOPP'],
     [
-      'https://i.imgur.com/TZEhCTX.png',
-      'https://i.imgur.com/WVAaMPA.png',
-      'https://i.imgur.com/pCMZeiM.png',
+      'QmPNEmaiWSaQhWYkjKBWctX5jwZ4t9WEPgssDNFwUT1RBx',
+      'QmNSa7MR5hcbJS1sHzx5AJ3HhHubChMYJhGGve7kJupii3',
+      'QmQ59urX6G91McKCha59vL7j9JsACCx9ofZKWJ5CT5cEYd',
     ],
     [100, 200, 300],
     [100, 50, 25],
